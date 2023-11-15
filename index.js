b9de8690dbd070a8ef18f36a69f5a293c225e002
Jeff Garelick <jeffrey.garelick@outlook.com>
2023-11-15 15:43:18 -0600

    complete lab

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === "hello")
+    return "I can\'t hear you!"
+else if (string ==="HELLO")
+return "YES INDEED!"
+else (string === "Let\'s have dinner together")
+return "I would love to!"
+}
